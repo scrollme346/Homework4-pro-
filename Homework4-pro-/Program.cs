6e2bdf1ea851d6017e7139620bf4fce6347e2530
scrol <scrol@DESKTOP-CDVGASJ>
2024-01-04 22:13:57 +0200

    Add project files.

---
+++ Homework4-pro-/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class Employee
+{
+    public string Name { get; set; }
+    public double Salary { get; set; }
+
+    public Employee(string name, double salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+
+     // Перегрузка оператора +
+    public static Employee operator +(Employee employee, double amount)
+    {
+        employee.Salary += amount;
+        return employee;
+    }
+
+    // Перегрузка оператора -
+    public static Employee operator -(Employee employee, double amount)
+    {
+        employee.Salary -= amount;
+        return employee;
+    }
+
+     // Перегрузка оператора ==
+    public static bool operator ==(Employee employee1, Employee employee2)
+    {
+        return employee1.Salary == employee2.Salary;
+    }
+
+    // Перегрузка оператора !=
+    public static bool operator !=(Employee employee1, Employee employee2)
+    {
+        return employee1.Salary != employee2.Salary;
+    }
+
+    // Перегрузка оператора >
+    public static bool operator >(Employee employee1, Employee employee2)
+    {
+        return employee1.Salary > employee2.Salary;
+    }
+
+    // Перегрузка оператора <
+    public static bool operator <(Employee employee1, Employee employee2)
+    {
+        return employee1.Salary < employee2.Salary;
+    }
+
+    // Перегрузка метода Equals
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Employee otherEmployee = (Employee)obj;
+        return this.Salary == otherEmployee.Salary;
+    }
+
+    // Перегрузка метода GetHashCode
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee employee = new Employee("John", 50000);
+
+        Console.WriteLine("Имя сотрудника: " + employee.Name);
+        Console.WriteLine("Зарплата сотрудника: " + employee.Salary);
+
+        employee += 5000;
+
+        Console.WriteLine("\nПосле изменения зарплаты");
+        Console.WriteLine("Зарплата сотрудника: " + employee.Salary);
+    }
+}
